473853349f6c28bcd827c5148ff761a3b582e1c2
Nathan <nathanlu_@hotmail.com>
2022-11-01 14:24:52 -0700

    Added posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import posthog from 'posthog-js'
+
 
 export default function App({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		posthog.init('phc_7Yz042GajExA7ykR6SgevdX5Idm6QOrs1Zw7DYuTrIJ', { api_host: 'https://app.posthog.com' })
+	}, [])
+
   return <Component {...pageProps} />
 }
